19e8d674acbf6cf9e9dc9c42f654a7245a20bc66
RiyaBanik <97799654+RiyaBanik@users.noreply.github.com>
2022-03-05 10:46:45 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,26 @@
+// import Moment from 'moment';
+var moment = require('moment');
+
+function fn1(Date1){
+    var Date1 = document.getElementById("date1").value;
+    myDate4 = moment("Date1").add(55,'days').format()
+    console.log(myDate4);
+}
+// let myDate;
+
+// myDate = moment().format()
+// myDate2 = moment().format("YYYY MMMM dddd")
+// myDate3 = moment().fromNow()
+// myDate4 = moment().add(55,'days').format()
+// var a = moment([2007, 0, 29]);
+// var b = moment([2007, 0, 28]);
+// myDate5 = a.diff(b, 'days') 
+// console.log(myDate2)
+// console.log(myDate3)
+// console.log(myDate4)
+// console.log(myDate5)
+// var Date2 = document.getElementById("date2").value;
+
+
+// myDate4 = moment("Date2").add(55,'days').format()
+// console.log(myDate4)
